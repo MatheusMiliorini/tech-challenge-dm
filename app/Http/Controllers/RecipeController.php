f1fdca166ab3957b7f06dbf425b1efe5a26ba87f
Matheus Miliorini <miliorinimatheus@gmail.com>
2021-02-23 18:49:39 -0300

    Inicialização do projeto

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+class RecipeController extends Controller {
+}
