5cfb83539cbe3bce6d709c9ef7cd07965e38df40
Matheus Miliorini <miliorinimatheus@gmail.com>
2021-02-24 21:18:05 -0300

    Corrigido formatação

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,67 +16,67 @@
       <template>
         <q-layout view="hHh lpR fFf">
 
-        <q-header elevated class="bg-primary text-white">
-          <q-toolbar>
-            <q-toolbar-title style="display: flex; align-items: center">
-              <img src="/img/logo_delivery_much.png" height="40px" style="margin-right: 15px">
-              Delivery Much Recipes
-            </q-toolbar-title>
-          </q-toolbar>
-        </q-header>
+          <q-header elevated class="bg-primary text-white">
+            <q-toolbar>
+              <q-toolbar-title style="display: flex; align-items: center">
+                <img src="/img/logo_delivery_much.png" height="40px" style="margin-right: 15px">
+                Delivery Much Recipes
+              </q-toolbar-title>
+            </q-toolbar>
+          </q-header>
 
-        <q-page-container>
-          <div class="row q-col-gutter-md q-mt-md q-px-md">
-            <!-- Busca -->
-            <div class="col-md-4">
-              <q-select
-                filled
-                v-model="selectedIngredients"
-                multiple
-                :options="ingredients"
-                label="Ingredients 👩‍🍳"
-                class="full-width"
-                counter
-                :max-values="3"
-                hint="Select up to three ingredients!"
-                use-input
-                fill-input
-                @filter="filterIngredients"
-                :debounce="0"
-              ></q-select>
+          <q-page-container>
+            <div class="row q-col-gutter-md q-mt-md q-px-md">
+              <!-- Busca -->
+              <div class="col-12 col-md-4">
+                <q-select
+                  filled
+                  v-model="selectedIngredients"
+                  multiple
+                  :options="ingredients"
+                  label="Ingredients 👩‍🍳"
+                  class="full-width"
+                  counter
+                  :max-values="3"
+                  hint="Select up to three ingredients!"
+                  use-input
+                  fill-input
+                  @filter="filterIngredients"
+                  :debounce="0"
+                ></q-select>
 
-              <q-select
-                filled
-                v-model="page"
-                :options="[1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]"
-                label="Page"
-                class="full-width q-mt-lg"
-              ></q-select>
+                <q-select
+                  filled
+                  v-model="page"
+                  :options="[1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]"
+                  label="Page"
+                  class="full-width q-mt-lg"
+                ></q-select>
 
-              <q-btn
-                :disable="selectedIngredients.length === 0"
-                label="Search! 🔍"
-                class="full-width q-mt-lg"
-                color="primary"
-                @click="searchRecipes"
-                :title="selectedIngredients.length === 0 ? 'You must select at least one ingredient' : ''"
-              ></q-btn>
-            </div>
+                <q-btn
+                  :disable="selectedIngredients.length === 0"
+                  label="Search! 🔍"
+                  class="full-width q-mt-lg"
+                  color="primary"
+                  @click="searchRecipes"
+                  :title="selectedIngredients.length === 0 ? 'You must select at least one ingredient' : ''"
+                ></q-btn>
+              </div>
 
-            <!-- Listagem -->
-            <div class="col-md-8">
-              <q-list class="recipe-list" v-if="recipes.length > 0">
-                <q-item v-for="(recipe, i) in recipes" :key="i">
-                  <recipe :recipe="recipe"></recipe>
-                </q-item>
-              </q-list>
-              <div v-else class="no-results text-grey-6">
-                <q-icon name="search"></q-icon>
-                <span>@{{ noRecipesText }}</span>
+              <!-- Listagem, aparece apenas em desktop -->
+              <div v-if="!$q.platform.is.mobile" class="col-md-8">
+                <q-list class="recipe-list" v-if="recipes.length > 0">
+                  <q-item v-for="(recipe, i) in recipes" :key="i">
+                    <recipe :recipe="recipe"></recipe>
+                  </q-item>
+                </q-list>
+                <div v-else class="no-results text-grey-6">
+                  <q-icon name="search"></q-icon>
+                  <span>@{{ noRecipesText }}</span>
+                </div>
               </div>
             </div>
-          </div>
-        </q-page-container>
+          </q-page-container>
 
         </q-layout>
       </template>
